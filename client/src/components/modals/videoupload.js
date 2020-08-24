e193fdf9de8f7191cd27069a0a20d60f91851914
neelamwaqar <waqar.maleen@gmail.com>
2020-08-25 02:18:25 +0500

    updated video model

--- client/src/components/modals/videoupload.js
+++ client/src/components/modals/videoupload.js
@@ -90,9 +90,9 @@ export class Videoupload extends Component {
                 />
                 <Button
                   onClick={this.onFileUpload}
-                  disabled={this.state.selectedFile}
+                  
                 >
-                  {this.state.selectedFile ? "Uploaded" : "Upload"}
+                  Upload
                 </Button>
               </div>
               {this.fileData()}
